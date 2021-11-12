677bc3db48a559eaae8c211430669ab529a88013
Owais <oowais@gmail.com>
2021-11-12 08:57:19 +0100

    Hide add task button on switching to about view

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'"
+    <Button v-show="homePage" @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'"
             :color="showAddTask? 'red' : 'green'"/>
   </header>
 </template>
@@ -12,6 +12,11 @@ import Button from '@/components/Button.vue';
 export default {
   name: "Header",
   components: {Button},
+  computed: {
+    homePage() {
+      return this.$route.path === '/';
+    }
+  },
   props: {
     title: {
       type: String,
