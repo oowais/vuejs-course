273c63bcda2e0549668017ac3d50a69cd840c60a
Owais <oowais@gmail.com>
2021-11-12 08:50:19 +0100

    added router to toggle between different views namely home and about

---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cont">
+    <h3>Version 1.0.0</h3>
+    <router-link to="/">Go back</router-link>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "About"
+}
+</script>
+
+<style scoped>
+.cont {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 4em;
+}
+</style>
