c643936f485ab0ddcfe86a75e406bba7b8027ecc
Owais <oowais@gmail.com>
2021-11-12 07:15:36 +0100

    Added header component added button component added tasks component added task component

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="onClick()" :style="{background: color}" class="btn">{{ text }}</button>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick(): void {
+      console.log('Clicked');
+    }
+  },
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green"/>
+  </header>
+</template>
+
+<script lang="ts">
+import Button from '@/components/Button.vue';
+
+export default {
+  name: "Header",
+  components: {Button},
+  props: {
+    title: {
+      type: String,
+      default: 'Default header value'
+    },
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 2em;
+}
+</style>
